0ec73ebe5082b904b149f49fa55192178fd5219a
meryemfelhi <42380565+meryemfelhi@users.noreply.github.com>
2018-08-17 15:56:48 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function italic(){ document.getElementById ("para").style.fontStyle="italic"}
+function bold(){ document.getElementById ("para").style.fontWeight="bold"}
+function sb(){ document.getElementById ("para").style.fontSize="100px"}
+function ss(){ document.getElementById ("para").style.fontSize="5px"}
+function f(){ document.getElementById ("para").innerHTML="Get out of my page now !!"}
+function underline(){ document.getElementById ("para").style.textDecoration="underline"}
+function reg(){ document.getElementById ("para").style.fontSize="40px"}
+document.getElementById("b1").style.fontSize="20px"
+document.getElementById("b2").style.fontSize="20px"
+document.getElementById("b3").style.fontSize="20px"
+document.getElementById("b4").style.fontSize="20px"
+document.getElementById("b5").style.fontSize="20px"
+document.getElementById("b6").style.fontSize="20px"
+document.getElementById("b7").style.fontSize="20px"
+function d1() { var x=document.getElementById("d1").value;
+document.getElementById("para").style.fontSize=x+"px"; }
+
+function d2() { var y=document.getElementById("d2").value;
+document.getElementById("para").style.fontFamily=y; }
+
+ 
